315b79824d32e5b2f374c12bb1c7313c08436301
Luiz Oliveira <luiz.oliveira@eficazmarketing.com>
2024-12-13 13:25:04 -0300

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,25 @@
 #include <iostream>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  do {
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+    // Uncomment this block to pass the first stage
+    std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  char *token = std::strtok(&input[0], " ");
+    std::string input;
+    std::getline(std::cin, input);
+    char *token = std::strtok(&input[0], " ");
 
-  char *command = token;
+    char *command = token;
 
-  if (input == "exit 0") {
-    return 0;
+    if (std::strcmp(command, "exit 0") == 0) {
+      return 0;
+      std::cout << input << ": command not found\n";
+    }
+
     std::cout << input << ": command not found\n";
-  }
-
-  std::cout << input << ": command not found\n";
+  } while (true);
 }
