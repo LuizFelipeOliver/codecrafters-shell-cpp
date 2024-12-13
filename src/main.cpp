7ba07e4c284d6b19e6bce74fae022bcfc7e425da
Luiz Oliveira <luiz.oliveira@eficazmarketing.com>
2024-12-13 14:25:10 -0300

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <valarray>
 
-enum commands { echo, cd, quit, invalid };
+enum commands { echo, cd, quit, type, invalid };
 
 commands string_to_commands(std::string str) {
   if (str.find("echo") != std::string::npos)
@@ -15,6 +16,9 @@ commands string_to_commands(std::string str) {
   if (str.find("exit") != std::string::npos)
     return quit;
 
+  if (str.find("type") != std::string::npos)
+    return type;
+
   return invalid;
 }
 
@@ -25,13 +29,22 @@ int main() {
 
   // Uncomment this block to pass the first stage
   std::string input = " ";
+
   while (!input.empty()) {
     std::cout << "$ ";
     std::getline(std::cin, input);
 
     switch (string_to_commands(input)) {
+
     case echo:
       std::cout << input.substr(5) << "\n";
+      break;
+
+    case type:
+      if (string_to_commands(input.substr(5)) != invalid)
+        std::cout << input.substr(5) << "is a shell builtin \n";
+      else
+        std::cout << input.substr(5) << " not found" << '\n';
       break;
 
     case quit:
