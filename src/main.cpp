10331fc170eecc5bf4e3421879b03feeebea7bfe
Luiz Oliveira <luiz.oliveira@eficazmarketing.com>
2024-12-16 17:28:17 -0300

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -71,11 +71,11 @@ int main() {
     case type:
       if (string_to_commands(arg) != invalid) {
         cout << arg << " is a shell builtin\n";
-      } else
-        (!path_command.empty()) {
-          cout << arg << " is " << path_command << std::endl;
-        }
-      cout << arg << ":" << not_found;
+      } else if (!path_command.empty()) {
+        cout << arg << " is " << path_command << std::endl;
+      } else {
+        cout << arg << ":" << not_found;
+      }
       break;
 
     case quit:
