39f17afaa856ef4aa3e0a836004232f62eb7657a
Luiz Oliveira <luiz.oliveira@eficazmarketing.com>
2024-12-18 09:11:50 -0300

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ vector<string> echoParse(const std::string &line) {
     char ch = line[i];
 
     if (escape_next) {
-      if (i + 1 < line.size() && line[i + 1] == ' ') {
+      if (i + 1 < line.size() && line[i + 1] == "  \\") {
         current_token.push_back(ch);
         i++;
         continue;
