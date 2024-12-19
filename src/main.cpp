1bf7ceaa7e2474be56814caf0223240e29cd8659
Luiz Oliveira <luiz.oliveira@eficazmarketing.com>
2024-12-19 11:00:34 -0300

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,7 @@ vector<string> echoParse(const string &line) {
       continue;
     }
 
-    if (inside_quotes && ch == '\\') {
+    if (!inside_quotes && ch == '\\') {
       escape_next = true;
       continue;
     }
